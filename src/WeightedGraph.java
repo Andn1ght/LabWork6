a7796204f9523535aa9b5d579397868cdfe35c57
Andn1ght <uzzhanibek.satigul2016@yandex.ru>
2023-05-31 19:37:21 +0600

    initializing public method 'addEdge' and helper method 'validateVertex'

--- src/WeightedGraph.java
+++ src/WeightedGraph.java
@@ -13,4 +13,17 @@ public class WeightedGraph<V> {
     public void addVertex(Vertex<V> vertex) {
         adjacencyList.put(vertex, new LinkedList<>());
     }
+
+    public void addEdge(Vertex<V> source, Vertex<V> destination, double weight) {
+        validateVertex(source);
+        validateVertex(destination);
+
+        source.addAdjacentVertex(destination, weight);
+        destination.addAdjacentVertex(source, weight);
+    }
+
+    private void validateVertex(Vertex<V> vertex) {
+        if (!adjacencyList.containsKey(vertex))
+            throw new IllegalArgumentException("Vertex " + vertex + " is not in the graph");
+    }
 }
