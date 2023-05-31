19b769a9234d15498011a7866098c583904d1fb8
Andn1ght <uzzhanibek.satigul2016@yandex.ru>
2023-05-31 17:47:11 +0600

    initializing the public constructor of this class

--- src/WeightedGraph.java
+++ src/WeightedGraph.java
@@ -1,6 +1,11 @@
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class WeightedGraph<V> {
     private Map<Vertex<V>, List<Vertex<V>>> adjacencyList;
+
+    public WeightedGraph() {
+        adjacencyList = new HashMap<>();
+    }
 }
