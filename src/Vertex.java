c10b0f77d7c6e034adf454b351a7b5057ee55644
Andn1ght <uzzhanibek.satigul2016@yandex.ru>
2023-05-31 17:43:13 +0600

    initializing the constructor of this class

--- src/Vertex.java
+++ src/Vertex.java
@@ -1,6 +1,12 @@
+import java.util.HashMap;
 import java.util.Map;
 
 public class Vertex<V> {
     private V data;
     private Map<Vertex<V>, Double> adjacentVertices;
+
+    public Vertex(V data) {
+        this.data = data;
+        this.adjacentVertices = new HashMap<>();
+    }
 }
